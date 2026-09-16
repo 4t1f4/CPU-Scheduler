a8a8d7f765fa967955bc5e6f78be09f2dd841cce
Atifa Sajida <satifa022006@gmail.com>
2026-09-16 23:50:27 +0530

    feat: add Flask backend and FCFS API

--- backend/app.py
+++ backend/app.py
@@ -1,7 +1,31 @@
-def main():
-    print("CPU Scheduler Simulator")
-    print("Backend is under development.")
+from flask import Flask, request, jsonify
+
+from algorithms.fcfs import fcfs
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "OS Algorithm Simulator Backend is Running!"
+
+
+@app.route("/api/cpu/fcfs", methods=["POST"])
+def run_fcfs():
+
+    # get data sent by frontend
+    data = request.get_json()
+
+    # get processes from the data
+    processes = data["processes"]
+
+    # run FCFS algorithm
+    result = fcfs(processes)
+
+    # send result back as JSON
+    return jsonify(result)
 
 
 if __name__ == "__main__":
-    main()
+    app.run(debug=True)
