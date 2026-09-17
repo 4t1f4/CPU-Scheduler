595948b97cc9c468cfc41d9e056239d611885d60
Atifa Sajida <satifa022006@gmail.com>
2026-09-17 12:19:37 +0530

    feat: add SJF CPU scheduling API

--- backend/routers/cpu.py
+++ backend/routers/cpu.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
+
 from algorithms.fcfs import fcfs
+from algorithms.sjf import sjf
 from schemas.process import FCFSRequest
 
 router = APIRouter()
 
 
+#this fcfs ka endpoint
 @router.post("/fcfs")
 def run_fcfs(data: FCFSRequest):
 
@@ -13,5 +16,18 @@ def run_fcfs(data: FCFSRequest):
 
     # run FCFS algorithm
     result = fcfs(processes)
+
+    return result
+
+
+#this sjf ka endpoint
+@router.post("/sjf")
+def run_sjf(data: FCFSRequest):
+
+    # convert Pydantic objects into dictionaries
+    processes = [process.model_dump() for process in data.processes]
+
+    # run SJF algorithm
+    result = sjf(processes)
 
     return result
