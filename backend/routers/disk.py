eed17d33b3432e876a786d66e4da8176ef7626dd
Atifa Sajida <satifa022006@gmail.com>
2026-09-17 19:42:02 +0530

    feat: add disk SSTF API endpoint

--- backend/routers/disk.py
+++ backend/routers/disk.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 
 from algorithms.disk_fcfs import disk_fcfs
+from algorithms.disk_sstf import sstf
+
 from schemas.disk import DiskRequest
 
 
@@ -12,5 +14,11 @@ router = APIRouter(
 @router.post("/fcfs")
 def run_disk_fcfs(data: DiskRequest):
     result = disk_fcfs(data.requests, data.head)
+
+    return result
+
+@router.post("/sstf")
+def run_sstf(data: DiskRequest):
+    result = sstf(data.requests, data.head)
 
     return result
