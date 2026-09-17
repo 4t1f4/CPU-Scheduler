89268014e2fb465312b1c7ec16c0ba25672bb258
Atifa Sajida <satifa022006@gmail.com>
2026-09-17 11:54:05 +0530

    feat: add Pydantic validation to CPU API

--- backend/routers/cpu.py
+++ backend/routers/cpu.py
@@ -1,14 +1,15 @@
 from fastapi import APIRouter
 from algorithms.fcfs import fcfs
+from schemas.process import FCFSRequest
 
 router = APIRouter()
 
 
 @router.post("/fcfs")
-def run_fcfs(data: dict):
+def run_fcfs(data: FCFSRequest):
 
-    # get processes from request
-    processes = data["processes"]
+    # convert Pydantic objects into dictionaries
+    processes = [process.model_dump() for process in data.processes]
 
     # run FCFS algorithm
     result = fcfs(processes)
